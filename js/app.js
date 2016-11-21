da42efc57673e38f1a86fe1cb7a94eb2b86eb09b
zoozeezoo <zoozeezoo@gmail.com>
2016-11-21 10:02:12 +0100

    js 2

--- js/app.js
+++ js/app.js
@@ -3,33 +3,47 @@ document.addEventListener("DOMContentLoaded", function() {
 
     var submenu = document.getElementsByClassName('submenu');
     var dropBtn = document.getElementsByClassName('dropBtn');
-    // var allSubmenuA = document.querySelectorAll('.dropBtn a')
-    console.log(submenu);
-    console.log(dropBtn);
 
-    dropBtn.addEventListener('click', function() {
-        submenu.classList.toggle('show');
+
+    dropBtn[0].addEventListener('click', function() {
+        submenu[0].classList.toggle('show');
     });
+
+
+    var allSubmenuA = document.querySelectorAll('.submenu a')
+    console.log(allSubmenuA);
+
+    window.onclick = function(e) {
+      if (!e.target.matches('.dropBtn')) {
+
+        for (var i = 0; i < allSubmenuA.length; i++) {
+          var submenuOpen = allSubmenuA[i];
+          if (submenuOpen.classList.contains('show')) {
+            submenuOpen.classList.remove('show');
+      }
+    }
+  }
+}
 
 
 
     var leftArr = document.getElementsByClassName('left-arrow');
     var rightArr = document.getElementsByClassName('right-arrow');
     var slideTab = document.getElementsByClassName('slide');
-    console.log(slideTab);
+    // console.log(slideTab);
 
     slideCounter = 0;
     slideTab[slideCounter].classList.add('show');
 
-    leftArr.addEventListener('click', function() {
-        slideTab[slideCounter].classList.remove('show');
-        slideCounter--;
-        if (slideCounter < slideTab.length) {
-            slideCounter = slideTab.length - 1;
-        }
-        slideTab[slideCounter].classList.add('show');
+      leftArr[0].addEventListener('click', function() {
+          slideTab[slideCounter].classList.remove('show');
+          slideCounter--;
+          if (slideCounter < slideTab.length) {
+              slideCounter = slideTab.length - 1;
+          }
+          slideTab[slideCounter].classList.add('show');
 
-        rightArr.addEventListener('click', function() {
+        rightArr[0].addEventListener('click', function() {
             slideTab[slideCounter].classList.remove('show');
             slideCounter++;
             if (slideCounter >= slideTab.length) {
